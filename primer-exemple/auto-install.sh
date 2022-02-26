42d663990f7146097f865cf654a626e78d996e12
yaizafontanet <57803576+yaizafontanet@users.noreply.github.com>
2022-02-26 19:28:44 +0100

    m08

--- primer-exemple/auto-install.sh
+++ primer-exemple/auto-install.sh
@@ -4,6 +4,24 @@ DOMAIN='insjdayf.hopto.org'
 apt update
 apt upgrade -y
 #instalar postfix y configurar Maildir
-bash ./postfix.sh
+apt update
+debconf-set-selections <<< "postfix postfix/mailname string insjdayf.hopto.org"
+debconf-set-selections <<< "postfix postfix/main_mailer_type string 'Internet Site'"
+apt-get install --assume-yes postfix
+cp /etc/postfix/main.cf /etc/postfix/main.cf.backup
+sudo postconf -e 'home_mailbox= Maildir/'
+sudo systemctl restart postfix.service
 #instalar dovecot y configurar
-bash ./dovecot.sh
+apt update
+apt install -y dovecot-core
+apt install -y dovecot-pop3d
+apt install -y dovecot-impad
+cp /etc/dovecot/conf.d/10-auth.conf /etc/dovecot/conf.d/10-auth.conf.backup
+sed -i '/^disable_plaintext_auth =.*/s/^/#/g' /etc/dovecot/conf.d/10-auth.conf
+echo "disable_plaintext_auth = no" >> /etc/dovecot/conf.d/10-auth.conf
+sed -i '/^auth_mechanisms =.*/s/^/#/g' /etc/dovecot/conf.d/10-auth.conf
+echo "auth_mechanisms = plain login" >> /etc/dovecot/conf.d/10-auth.conf
+cp /etc/dovecot/conf.d/10-mail.conf /etc/dovecot/conf.d/10-mail.conf.backup
+sed -i '/^mail_location =.*/s/^/#/g' /etc/dovecot/conf.d/10-mail.conf
+echo "mail_location = mailbox:~/Maildir" >> /etc/dovecot/conf.d/10-mail.conf
+systemctl restart dovecot.service
