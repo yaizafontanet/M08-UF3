af3d95ca326c4b77475f07aca14a7c446ffafd6e
yaizafontanet <57803576+yaizafontanet@users.noreply.github.com>
2022-02-28 10:48:54 +0100

    m08

--- primer-exemple/auto-install.sh
+++ primer-exemple/auto-install.sh
@@ -14,9 +14,7 @@ sudo systemctl restart postfix.service
 #instalar dovecot y configurar
 apt update
 apt install -y dovecot-core
-apt update
 apt install -y dovecot-pop3d
-apt update
 apt install -y dovecot-impad
 cp /etc/dovecot/conf.d/10-auth.conf /etc/dovecot/conf.d/10-auth.conf.backup
 sed -i '/^disable_plaintext_auth =.*/s/^/#/g' /etc/dovecot/conf.d/10-auth.conf
@@ -26,4 +24,5 @@ echo "auth_mechanisms = plain login" >> /etc/dovecot/conf.d/10-auth.conf
 cp /etc/dovecot/conf.d/10-mail.conf /etc/dovecot/conf.d/10-mail.conf.backup
 sed -i '/^mail_location =.*/s/^/#/g' /etc/dovecot/conf.d/10-mail.conf
 echo "mail_location = mailbox:~/Maildir" >> /etc/dovecot/conf.d/10-mail.conf
+apt install -y dovecot-impad
 systemctl restart dovecot.service
