d7f86ad49d32a655b61195efe1e206ea14b57830
yaizafontanet <57803576+yaizafontanet@users.noreply.github.com>
2022-02-28 19:14:07 +0100

    m08

--- primer-exemple/auto-install.sh
+++ primer-exemple/auto-install.sh
@@ -119,8 +119,9 @@ systemctl restart apache2.service
 
 #Configurar archivo de netplan
 cp /etc/netplan/50-cloud-init.yaml /etc/netplan/50-cloud-init.yaml.backup
-sed -n 'H;${x;s/^\n//;s/set-name: eth0 .*$/nameservers:\n&/;p;}' /etc/netplan/50-cloud-init.yaml
-sed -n 'H;${x;s/^\n//;s/nameservers: .*$/addresses: [$IP]\n&/;p;}' /etc/netplan/50-cloud-init.yaml
+ed /etc/netplan/50-cloud-init.yaml < ${file("script1.ed")}
+ed /etc/netplan/50-cloud-init.yaml < ${file("script2.ed")}
+netplan apply
 
 #instalar y configurar bind9
 apt update
@@ -159,7 +160,7 @@ echo ""$TTL"    604800
 @               IN      NS      $ME.$DOMAIN.
 @               IN      A       $IP
 @               IN      AAAA    ::1
-$ME IN      A       $IP
+$ME    IN      A       $IP
     
 webmail         IN      CNAME   $ME
 " > /etc/bind/forward.$DOMAIN 
