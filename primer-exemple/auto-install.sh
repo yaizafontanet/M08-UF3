075a84797ceaf7935d5acd231d77a9bf46b0c234
yaizafontanet <57803576+yaizafontanet@users.noreply.github.com>
2022-03-01 14:33:54 +0100

    m08

--- primer-exemple/auto-install.sh
+++ primer-exemple/auto-install.sh
@@ -119,8 +119,20 @@ systemctl restart apache2.service
 
 #Configurar archivo de netplan
 cp /etc/netplan/50-cloud-init.yaml /etc/netplan/50-cloud-init.yaml.backup
-ed /etc/netplan/50-cloud-init.yaml < ${file("script1.ed")}
-ed /etc/netplan/50-cloud-init.yaml < ${file("script2.ed")}
+ed /etc/netplan/50-cloud-init.yaml << EOF
+i13
+            nameservers:
+.
+w
+q
+EOF
+ed /etc/netplan/50-cloud-init.yaml << EOF
+i14
+                addresses: [$IP]
+.
+w
+q
+EOF
 netplan apply
 
 #instalar y configurar bind9
